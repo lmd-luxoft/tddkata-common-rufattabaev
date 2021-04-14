c734219c078629ce56593dbff8751eb3154f2efa
TabaevRI <rufattabaev636@gmail.com>
2021-04-14 11:11:15 +0300

    task 4 complete

--- src/main/java/tddkata/Calc.java
+++ src/main/java/tddkata/Calc.java
@@ -10,8 +10,24 @@ public class Calc {
         if (expression.equals("")) {
             return result;
         }
-        String[] values = expression.split("[,\\n]");
+        if (expression.startsWith("//")) {
+            char customDelimiter = expression.charAt(2);
+            String targetString = expression.substring(expression.indexOf("\n") + 1);
+            String[] valuesWithCustomDelimiter = separateString(targetString, Character.toString(customDelimiter));
+            return countValuesSum(valuesWithCustomDelimiter);
 
+        } else {
+            String[] valuesWithDefaultDelimiter = expression.split("[,\\n]");
+            return countValuesSum(valuesWithDefaultDelimiter);
+        }
+    }
+
+    private String[] separateString(String expression, String delimiter) {
+        return expression.split(delimiter);
+    }
+
+    private int countValuesSum(String[] values) {
+        int result = 0;
         for (String value : values) {
             try {
                 int intValue = Integer.parseInt(value);
